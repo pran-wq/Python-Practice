e27ab4bb4f4a30341e05d4a3f93d009ed950be13
Pranav Raj <prq1232007@gmail.com>
2025-10-03 20:57:27 +0530

    Add chapter1.py

---
+++ calculatar.py
@@ -0,0 +1,23 @@
+# a= (int(input("enter the value of a: ",)))
+# b= (int(input("enter the value of b: ",)))
+# print("the sum of a and b is = ", a+b)
+# print("the difference of a and b =", a-b)
+# print("the product of a and b  =",a*b)
+# print("the quiotent of a and b =",a/b)
+# print('''winkle, twinkle, little star,
+# How I wonder what you are!
+# Up above the world so high,
+# Like a diamond in the sky.
+# Twinkle, twinkle, little star,
+# How I wonder what you are!
+# When the blazing sun is gone,
+# When he nothing shines upon,
+# Then you show your little light,
+# Twinkle, twinkle, all the night.
+# Twinkle, twinkle, little star,
+# How I wonder what you are!''')
+import pyjokes
+joke = pyjokes.get_joke()
+print("today's joke is")
+print(joke)
+    
---
+++ chapter1.py
@@ -0,0 +1,7 @@
+print("hello world from chapter 1")
+print("what is your name?")
+name = input()
+print("hello,"+name + "!")
+print("the length of your name is ") 
+print(len(name))
+
