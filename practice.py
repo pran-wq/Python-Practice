1563d230de42573292f6c3c732e2531b0e6b1cf7
PRANAV RAJ <114560034+pran-wq@users.noreply.github.com>
2025-11-20 19:23:59 +0530

    Add files via upload
    my practice on recrussive use of function.

---
+++ practice.py
@@ -0,0 +1,22 @@
+#function for calculating factorial
+# def fact(a):
+#     if a == 0 or a == 1:
+#         return 1
+#     fact_val = 1
+#     for i in range (1,a+1):
+#         fact_val *= i
+#     return fact_val
+# print(fact(0))
+
+# #second methodfunction for calculating factorial
+# def fact(a):
+#     if a == 0 or a == 1:
+#         return 1
+#     else:
+#         return a *fact(a-1)
+    
+# a = int(input ("enter the number: "))
+# print (fact(a))
+    
+pi = 3.14159
+print("the value of pi is: %g" %pi )
